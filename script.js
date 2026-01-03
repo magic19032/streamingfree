7779fdb53f7b678ab8d606a3ef5553718e76a099
Game_master19032 <Game_master19032@outlook.fr>
2026-01-03 16:56:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("h1").onclick = function() {
+    alert("Welcome to StreamingFree!");
+}
+document.getElementById("ImagePlanning").onclick = function() {
+    window.location.href = "planning.html";
+}
