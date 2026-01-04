a8dd9e6b51b2fd221fc95c9defeba15bef3d0ae4
Game_master19032 <Game_master19032@outlook.fr>
2026-01-04 20:20:23 +0100

    Refactor click handlers and add console log

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.getElementById("#h1").onclick = function() {
     alert("Welcome to StreamingFree!");
-};
+}
+console.log("hello 1");
 document.querySelector("#PlanningAnime").onclick = function() {
     window.location.href = "Planning.html";
-};
+}
 document.querySelector("#PlanningFilm").onclick = function() {
     window.location.href = "Planning.html";
-};
+}
 document.querySelector("#PlanningSerie").onclick = function() {
     window.location.href = "Planning.html";
-};
-console.log("Hello");
+}
+console.log("Hello")
+
