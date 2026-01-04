041a01cfd6a0427cbb1150b100214a25d58e6119
Game_master19032 <Game_master19032@outlook.fr>
2026-01-04 20:18:34 +0100

    Reformat script.js for consistency

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById("#h1").onclick = function() {
     alert("Welcome to StreamingFree!");
-}
+};
 document.querySelector("#PlanningAnime").onclick = function() {
     window.location.href = "Planning.html";
-}
+};
 document.querySelector("#PlanningFilm").onclick = function() {
     window.location.href = "Planning.html";
-}
+};
 document.querySelector("#PlanningSerie").onclick = function() {
     window.location.href = "Planning.html";
-}
+};
 console.log("Hello");
